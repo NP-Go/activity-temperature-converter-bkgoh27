1d1e2ac3f39b891019f699001eadb551aff85f8f
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 11:51:18 +0800

    convert temperature with if else function calling

--- temperatureConverter.go
+++ temperatureConverter.go
@@ -3,47 +3,44 @@ package main
 import "fmt"
 
 func convertKelvin(temperatureInput float64) (float64, float64) {
-	//Insert your code here
-
+	var resultFahrenheit = 9.0 / 5.0 * (temperatureInput - 459.67)
+	var resultCelsius = 5.0 / 9.0 * (resultFahrenheit - 32.0)
 	//Do not remove this line
 	return resultFahrenheit, resultCelsius
 }
 
 func convertCelsius(temperatureInput float64) (float64, float64) {
-	//Insert your code here
-
+	var resultFahrenheit = 9.0 / 5.0 * (temperatureInput + 32.0)
+	var resultKelvin = 5.0 / 9.0 * (resultFahrenheit + 459.67)
 	//Do not remove this line
 	return resultFahrenheit, resultKelvin
 }
 
 func convertFahrenheit(temperatureInput float64) (float64, float64) {
-	//Insert your code here
-
+	var resultKelvin = 5.0 / 9.0 * (temperatureInput + 459.67)
+	var resultCelsius = 5.0 / 9.0 * (temperatureInput - 32.0)
 	//Do not remove this line
 	return resultKelvin, resultCelsius
 }
 
 func main() {
 	var temperatureChoice int
-	var temperatureInput float64
-	fmt.Println("Enter your temperature format (1 for Kelvin, 2 for Celsius, 3 for Fahrenheit: ")
+	var temperatureInput, resultFahrenheit, resultCelsius, resultKelvin float64
+	fmt.Println("Enter your temperature format (1 for Kelvin, 2 for Celsius and 3 for Fahrenheit.): ")
 	fmt.Scanln(&temperatureChoice)
 	fmt.Println("Enter the temperature: ")
 	fmt.Scanln(&temperatureInput)
 
 	if temperatureChoice == 1 {
-		//Insert Code here
-
+		resultFahrenheit, resultCelsius = convertKelvin(temperatureInput)
 		//DO not remove this
 		fmt.Println("Fahrenheit: ", resultFahrenheit, " Celsius: ", resultCelsius)
 	} else if temperatureChoice == 2 {
-		//Insert Code here
-
+		resultFahrenheit, resultKelvin = convertCelsius(temperatureInput)
 		//DO not remove this
 		fmt.Println("Fahrenheit: ", resultFahrenheit, " Kelvin: ", resultKelvin)
 	} else if temperatureChoice == 3 {
-		//Insert Code here
-
+		resultKelvin, resultCelsius = convertFahrenheit(temperatureInput)
 		//DO not remove this
 		fmt.Println("Kelvin: ", resultKelvin, " Celsius: ", resultCelsius)
 	} else {
